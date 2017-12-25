73ac1e6089165819e9cfeb113a820df7cecaf2ad
LinnikPolina <apoliina@outlook.com>
2017-12-25 09:21:32 +0300

    django

--- init.sh
+++ init.sh
@@ -1,12 +1,6 @@
-if [ -f /etc/nginx/sites-enabled/default ]; then
-  sudo rm /etc/nginx/sites-enabled/default
-fi
-
-# Nginx
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-
-# Gunicorn
-sudo ln -sf /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
