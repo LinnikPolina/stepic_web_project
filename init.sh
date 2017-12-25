b9da326e9e1f2405ed6b516c216485a401c3f539
LinnikPolina <apoliina@outlook.com>
2017-12-25 09:03:21 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
-
 if [ -f /etc/nginx/sites-enabled/default ]; then
   sudo rm /etc/nginx/sites-enabled/default
 fi
 
+# Nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+# Gunicorn
+sudo ln -sf /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
