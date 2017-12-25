98c175a6f8bb981fcb79f86130fe12ffeb93d8e2
LinnikPolina <apoliina@outlook.com>
2017-12-25 09:28:57 +0300

    qa

--- init.sh
+++ init.sh
@@ -1,6 +1,14 @@
-sudo rm /etc/nginx/sites-enabled/default
+#!/bin/bash
+
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+
+# Nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+
+# Gunicorn
+sudo ln -sf /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
